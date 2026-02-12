19d099373217ea874b057f2ff7ae1c117a01bed2
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-02-12 09:29:43 +0100

    Button practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import Button from './Button.jsx'
 import Card from './Card.jsx'
 
 function App() {
   return (
     <>
     <Card/>
-
+    <Button/>
     </>
   );
 }
---
+++ src/Button.jsx
@@ -0,0 +1,18 @@
+
+function Button(){
+   const styles = {
+  backgroundColor:"hsl(256, 51%, 36%)",
+  color:"white",
+  padding: "10px 20px",
+  borderRadius: "5px",
+  border: "none",
+  cursor: "pointer",
+   }
+
+
+ return(
+    <button style = {styles}>Click me</button>
+ )
+}
+
+export default Button
