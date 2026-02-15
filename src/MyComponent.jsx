08c9d0e5d21a557d27158779ecbf21347c0a6acb
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-02-15 13:06:49 +0100

    Setters example 1

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,6 +1,32 @@
+import React, {useState} from 'react';
 
 function MyComponent() {
 
+const [name, setName] = useState("Guest");
+const [age, setAge] = useState(0);
+const [isEmployed, setIsEmployed] = useState(false);
+
+const updateName = () => {
+    setName('Petar');
 }
 
-export default MyComponent;
+const incrementAge = () => {
+    setAge(age +1);
+}
+
+
+return(
+
+        <div>
+             <p>Name: {name}</p>
+            <button onClick={updateName}>Set name:</button>
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>increment Age:</button>
+
+            <p>Employed {isEmployed ? "Yes" : "No"}</p>
+            <button onClick={() => setIsEmployed(!isEmployed)}>Toggle Employment Status</button>
+        </div>  );
+
+}
+
+export default MyComponent
