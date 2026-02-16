a2171b03b95d6210062d4de0b4341dfcb7c1f2e6
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-02-16 09:55:39 +0100

    Added an order form clone

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,7 +3,9 @@ import React, {useState} from 'react';
 function MyComponent() {
 
 const [name, setName] = useState("Ime");
-const [quantity, setQuantity] = useState("kolicina");
+const [quantity, setQuantity] = useState(1);
+const [comment, setComment] = useState();
+const [payment, setPayment] = useState("");
 
 
     function handleNameChange(event){
@@ -14,17 +16,34 @@ const [quantity, setQuantity] = useState("kolicina");
         setQuantity(event.target.value);
     }
 
+    function handleCommentChange(event) {
+        setComment(event.target.value);
+    }
+
+    function handlePaymentChange(event) {
+        setPayment(event.target.value);
+    }
+
 return(
 
         <div>
             <input value={name} onChange={handleNameChange}></input>
-            
             <p>Name: {name}</p>
 
-
-
-            <input value={quantity} onChange={handleQuantityChange}></input>
+            <input value={quantity} onChange={handleQuantityChange} type="number"></input>
             <p>Quantity: {quantity}</p>
+
+            <textarea value={comment} onChange={handleCommentChange} 
+            placeholder="Enter your comment here..."></textarea>
+            <p>Comment: {comment}</p>
+
+            <select value={payment} onChange={handlePaymentChange}>
+                <option>Select an option</option>
+                <option value="Preuzecem">Preuzecem</option>
+                <option value="Placanje karticom">Placanje karticom</option>
+                <option value="Kripto">Kripto</option>
+            </select>
+            
         </div> 
 
      );
