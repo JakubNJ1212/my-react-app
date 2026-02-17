3fa0ed36201668a48f4a15a330e09f2ee64db598
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-02-17 13:44:58 +0100

    Added an updater to array

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -6,19 +6,24 @@ const [foods, setFoods] = useState(["Apple"]);
 
 
 function handleAddFood() {
-    
+    const newFood = document.getElementById("foodInput").value;
+    document.getElementById("foodInput").value = "";
+    setFoods(f => [...f, newFood])
 }
 
-function handleRemoveFood() {
-    
+function handleRemoveFood(index) {
+    setFoods(foods.filter((_,i) => i !== index));
 }
 
 
 return(<>
     <div>
-        
-     
-
+            <h2>Food list</h2> 
+            <ul>
+                {foods.map((food, index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
+            </ul>
+            <input type="text" id="foodInput" placeholder="Enter some food"/>
+    <button onClick={handleAddFood}>Add food</button>
     </div>
 
 
