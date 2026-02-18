058a095ec06f45acb8f1eaa8e3514e1dd2ed83f9
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-02-18 13:42:27 +0100

    practicing how to update array of objects

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,31 +2,12 @@ import React, {useState} from 'react';
 
 function MyComponent() {
 
-const [foods, setFoods] = useState(["Apple"]);
-
-
-function handleAddFood() {
-    const newFood = document.getElementById("foodInput").value;
-    document.getElementById("foodInput").value = "";
-    setFoods(f => [...f, newFood])
-}
-
-function handleRemoveFood(index) {
-    setFoods(foods.filter((_,i) => i !== index));
-}
 
 
 return(<>
     <div>
-            <h2>Food list</h2> 
-            <ul>
-                {foods.map((food, index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
-            </ul>
-            <input type="text" id="foodInput" placeholder="Enter some food"/>
-    <button onClick={handleAddFood}>Add food</button>
+            
     </div>
-
-
 </>
  );
 
