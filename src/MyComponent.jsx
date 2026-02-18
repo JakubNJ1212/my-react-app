a78fb07e98e857c098533269073306b6daf9520d
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-02-18 14:13:24 +0100

    Added handlers/update for objects

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,11 +2,37 @@ import React, {useState} from 'react';
 
 function MyComponent() {
 
+    const [cars, setCars] = useState([]);
+    const [carYear, setCarYear] = useState(new Date().getFullYear());
+    const [carMake, setCarMake] = useState();
+    const [carModel, setCarModel] = useState();
+
+
+    function handleAddCar() {
+
+    }
+
+    function handleRemoveCar(index) {
+
+    }
+
+    function handleYearChange(event) {
+
+    }
+    
+    function handleMakeChange(event) {
+        
+    }
+
+     function handleModelChange(event) {
+        
+    }
+
 
 
 return(<>
     <div>
-            
+         <h2>List of car objects</h2>   
     </div>
 </>
  );
