284f2af7d3e4784fe7a152b69929967a13158253
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-02-24 18:58:10 +0100

    Added an useEffect and IntervalID

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -2,6 +2,12 @@ import React, {useState, useEffect} from 'react';
 
 function DigitalClock() {
 
+    const [time, setTime] = useState(new Date())
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+    }, []);
 
     return(
     <div className="clock-container" >
