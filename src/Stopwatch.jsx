28f8dd017752903bff6285338dd2fb6cc16599c7
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-03-04 16:04:48 +0100

    Added elapsed time const and useRef

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -4,8 +4,9 @@ import React, {useState, useRef, useEffect} from 'react'
 
 function Stopwatch() {
 
-     const [isRunning, seIsRunning] = useState(false)
-
+     const [isRunning, seIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
 
     return(<>
    
