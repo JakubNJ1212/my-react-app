72eda293167a9f062b95cd19ad47c6cc6a2d8175
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-03-04 16:12:16 +0100

    Added div elements and a placeholder for time

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -7,9 +7,38 @@ function Stopwatch() {
      const [isRunning, seIsRunning] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
     const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+
+    useEffects(() => {
+
+
+    }, [isRunning]) ;
+
+    function start() {
+         
+    }
+
+    function stop() {
+
+    }
+
+    function reset() {
+
+    }
+
+    function formatTime() {
+        return `00:00:00`;
+    }
 
     return(<>
-   
+   <div className="stopwatch">
+        <div className="display">
+            {formatTime}
+        </div>
+
+
+   </div>
 
     </>);
 }
