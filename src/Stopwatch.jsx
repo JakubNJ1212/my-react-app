07802399860a1d3c31221a20f5bd25f966493dec
Jakub Njemoga <jakub01nemoga@gmail.com>
2026-03-04 16:01:17 +0100

    Made a state that checks if the stopwatch is running

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -4,5 +4,12 @@ import React, {useState, useRef, useEffect} from 'react'
 
 function Stopwatch() {
 
+     const [isRunning, seIsRunning] = useState(false)
+
+
+    return(<>
+   
+
+    </>);
 }
 export default Stopwatch
